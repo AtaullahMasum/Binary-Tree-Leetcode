40abd75bc613749aa804ad6b12f5bb1b58e854f6
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-18 11:58:16 +0600

    kth largest sum in level

---
+++ 2583. Kth Largest Sum in a Binary Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+        queue = [root]
+        result = []
+        
+        while queue:
+            level_sum = 0
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level_sum += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level_sum)
+        result.sort(reverse= True)
+        return result[k-1] if len(result) > k-1 else -1
