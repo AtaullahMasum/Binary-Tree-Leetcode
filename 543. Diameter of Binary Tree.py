0f909ccb6fd7544c39d4755b544028e1945fde28
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-18 23:16:44 +0600

    Optimal Solution

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -25,3 +25,17 @@ class Solution:
         maximum = [0]
         self.diameter(root, maximum)
         return maximum[0]
+#Optimal Approach
+class Solution:
+    def height (self, root, maximum):
+        if not root:
+            return 0
+        left = self.height(root.left, maximum)
+        right = self.height(root.right, maximum)
+        maximum[0] = max(maximum[0], left+right)
+        return 1 + max(left, right)
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        maximum = [0]
+        self.height(root, maximum)
+        return maximum[0]
+
