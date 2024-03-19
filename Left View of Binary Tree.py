6e38f4ecb4dc9ad21b221d28b2be881ea2deb8d0
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-19 21:48:27 +0600

    recursive solution added

--- Left View of Binary Tree.py
+++ Left View of Binary Tree.py
@@ -6,7 +6,7 @@ class Node:
         self.left = None
         self.right = None
 '''
-
+#using iteratively or BFS
 #Function to return a list containing elements of left view of the binary tree.
 def LeftView(root):
     
@@ -25,4 +25,19 @@ def LeftView(root):
             if node.right:
                 queue.append(node.right)
     return result
-        
+#using recursivly or DFS 
+def preorder(root, level, result):
+    if not root:
+        return
+    if level == len(result):
+        result.append(root.data)
+    preorder(root.left, level+1, result)
+    preorder(root.right, level+1, result)
+def LeftView(root):
+    
+    # code here
+    result = []
+    if not root:
+        return result
+    preorder(root, 0, result)
+    return result
