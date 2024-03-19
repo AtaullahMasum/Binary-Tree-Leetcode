063381c9531d37e776dac2bb94a9b0c5808826a3
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-19 16:23:44 +0600

    using preorder traversal

--- 987. Vertical Order Traversal of a Binary Tree.py
+++ 987. Vertical Order Traversal of a Binary Tree.py
@@ -22,3 +22,29 @@ class Solution:
         for _, nodes in sorted( vertical_levels.items(), key=lambda item: item[0]):
            result.append([val for _, val in sorted(nodes)])  
         return result
+
+#Using DFS (preorder traversal)
+from collections import defaultdict
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorder(self, root, col, row, vertical_levels):
+        if not root:
+            return
+        vertical_levels[col].append((row, root.val))
+        self.preorder(root.left, col-1, row+1, vertical_levels)
+        self.preorder(root.right, col+1, row+1, vertical_levels)
+        
+    def verticalTraversal(self, root):
+        if not root:
+            return []        
+        vertical_levels = defaultdict(list)
+        self.preorder(root,0,0,vertical_levels)
+        result = []
+        for _ , nodes in sorted(vertical_levels.items(), key = lambda item:item[0]):
+            result.append([val for _, val in sorted(nodes)])
+        return result
