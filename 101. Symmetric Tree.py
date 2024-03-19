79b0a45bae5f1300a9ae0d3eb6bb0a203b22a2dd
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-19 22:39:07 +0600

    check symmetric binary

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetricHelp(self,root1, root2):
+        if not root1 or not root2:
+            return root1 == root2
+        if root1.val != root2.val:
+            return False
+        return self.isSymmetricHelp(root1.left, root2.right) and self.isSymmetricHelp(root1.right, root2.left)
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return not root or self.isSymmetricHelp(root.left, root.right)
+        
