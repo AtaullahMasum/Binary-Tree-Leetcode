2a3659a7570eb590729dc181847bf450910246fc
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-20 19:32:39 +0600

    All Nodes Distance K in Binary Tree

---
+++ 863. All Nodes Distance K in Binary Tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        parent = {}
+        self.populateParent(root, None, parent)
+        
+        queue = deque([(target, 0)])
+        visited = set([target])
+        result = []
+        
+        while queue:
+            node, dist = queue.popleft()
+            if dist == k:
+                result.append(node.val)
+            elif dist > k:
+                break
+            
+            for child in (node.left, node.right, parent[node]):
+                if child and child not in visited:
+                    visited.add(child)
+                    queue.append((child, dist + 1))
+        
+        return result
+    
+    def populateParent(self, root, par, parent):
+        if root:
+            parent[root] = par
+            self.populateParent(root.left, root, parent)
+            self.populateParent(root.right, root, parent)
