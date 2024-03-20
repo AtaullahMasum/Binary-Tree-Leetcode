9b8da004ed19f021b6c248c84a89bd0fb0e29c89
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-20 19:57:37 +0600

    both bfs and dfs solution added

--- 863. All Nodes Distance K in Binary Tree.py
+++ 863. All Nodes Distance K in Binary Tree.py
@@ -6,6 +6,7 @@
 #         self.right = None
 from collections import deque
 from types import List, TreeNode
+#DFS and BFS solution
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         parent = {}
@@ -34,3 +35,34 @@ class Solution:
             parent[root] = par
             self.populateParent(root.left, root, parent)
             self.populateParent(root.right, root, parent)
+
+#BFS and BFS Solution
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        parent = {}
+        parent[root] = None
+        queue = [root]
+        while queue:
+            node = queue.pop(0)
+            if node.left:
+                queue.append(node.left)
+                parent[node.left] = node
+            if node.right:
+                queue.append(node.right)
+                parent[node.right] = node 
+        queue =[(target, 0)]
+        visited = set()
+        visited.add(target)
+        result = []
+        while queue:
+            node, dist = queue.pop(0)
+            if dist == k:
+                result.append(node.val)
+            elif dist > k:
+                break
+            for child in (node.left, node.right, parent[node]):
+                if child and child not in visited:
+                    visited.add(child)
+                    queue.append((child, dist+1))
+        return result
+
