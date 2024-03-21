18b089a9943127a048263eaaa2eaacf827228a8c
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-21 14:30:13 +0600

    optimal Solution added

--- 106. Construct Binary Tree from Inorder and Postorder Traversal.py
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -16,4 +16,24 @@ class Solution:
         root.left = self.buildTree(inorder[:root_index], postorder)
        
         return root
+#using more optimized method
+class Solution:
+     def buildTreeInPost(self, inorder, inStart, inEnd, postorder, postStart, postEnd):
+        # Base case: If there's no elements to construct the subtree
+        if inStart > inEnd or postStart > postEnd:
+            return None
+
+        # Last element in postorder is the root of the current subtree
+        root = TreeNode(postorder[postEnd])
+        # Finding the root in inorder to separate left and right subtrees
+        inRoot = inorder.index(root.val)
+        # Number of nodes in the left subtree
+        numLeft = inRoot - inStart
+
+        # Construct left subtree
+        root.left = self.buildTreeInPost(inorder, inStart, inRoot - 1, postorder, postStart, postStart + numLeft - 1)
+        # Construct right subtree
+        root.right = self.buildTreeInPost(inorder, inRoot + 1, inEnd, postorder, postStart + numLeft, postEnd - 1)
+
+        return root
         
