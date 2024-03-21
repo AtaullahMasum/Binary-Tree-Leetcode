9de583007a656e609a3cd4611153a7840a35c506
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-21 13:06:46 +0600

    using hashing added

--- 105. Construct Binary Tree from Preorder and Inorder Traversal.py
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -15,4 +15,32 @@ class Solution:
         root.left = self.buildTree(preorder, inorder[:root_index])
         root.right = self.buildTree(preorder, inorder[root_index+1:])
         return root
+#Using Hashing
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import OrderedDict
+class Solution:
+    def buildTreePreIn(self, preorder, preStart, preEnd, inorder, inStart, inEnd, ordermap):
+        if preStart > preEnd or inStart > inEnd:
+            return None
+        root = TreeNode(preorder[preStart])
+        inRoot = ordermap[root.val]
+        numsLeft = inRoot - inStart
+        root.left = self.buildTreePreIn(preorder, preStart+1, preStart+numsLeft, inorder, inStart, inRoot-1, ordermap )
+        root.right = self.buildTreePreIn(preorder, preStart+numsLeft+1, preEnd, inorder, inRoot+1, inEnd, ordermap)
+        return root
+
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        ordermap = OrderedDict()
+        for i, value in enumerate(inorder):
+            ordermap[value] = i
+        for i in range(len(inorder)):
+            ordermap[inorder[i]] = i
+        root = self.buildTreePreIn(preorder, 0, len(preorder)-1, inorder, 0, len(inorder)-1, ordermap)
+        return root
         
