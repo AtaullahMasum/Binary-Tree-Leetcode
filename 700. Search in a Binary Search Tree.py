6483b13d1699621d37ccb358b3a2c5bfcc2b3466
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-22 13:49:32 +0600

    recursive solution added

--- 700. Search in a Binary Search Tree.py
+++ 700. Search in a Binary Search Tree.py
@@ -12,3 +12,13 @@ class Solution:
             else:
                 root = root.left
         return root
+#Using Recursively
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root or root.val == val:
+            return root
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+        
