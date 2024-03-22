4bd83dd788a92c3b181ffa8af869d4129f7b5434
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-23 04:57:40 +0600

    valid BST check

---
+++ 98. Validate Binary Search Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorder(self, root, min, max):
+        if not root:
+            return True
+        if  not min <root.val< max:
+            return False
+        return self.preorder(root.left, min, root.val) and self.preorder(root.right, root.val, max)
+    
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.preorder(root, float('-inf'), float('inf'))
