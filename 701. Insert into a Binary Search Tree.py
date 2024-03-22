ba0865c88aaaae870ce65ac43cd45fb7441fff58
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-22 19:23:00 +0600

    iterative solution added

--- 701. Insert into a Binary Search Tree.py
+++ 701. Insert into a Binary Search Tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from typing import Optional
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root:
@@ -13,4 +14,23 @@ class Solution:
         else:
             root.left = self.insertIntoBST(root.left, val)
         return root
-        
+#Iterative solution
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        curr = root
+        while curr:
+            if val >= curr.val:
+                if curr.right:
+                    curr = curr.right
+                else:
+                    curr.right = TreeNode(val)
+                    break
+            else:
+                if curr.left:
+                    curr = curr.left
+                else:
+                    curr.left = TreeNode(val)
+                    break
+        return root
