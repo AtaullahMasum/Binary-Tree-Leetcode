af6885fd9fa412f816630426086511fca541c322
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-23 00:00:05 +0600

    recursive solution added

--- 450. Delete Node in a BST.py
+++ 450. Delete Node in a BST.py
@@ -80,3 +80,28 @@ class Solution:
                 else:
                     current = current.right
         return root
+#Using recursive solution 
+class Solution:
+    
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left and not root.right:
+                return None
+            elif not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                temp = root.right
+                while temp.left:
+                    temp= temp.left
+                temp.left = root.left
+                return root.right
+        return root
+        
