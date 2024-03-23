91e9e8218824a7faa53afe99947ddb5c031e848b
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-23 17:55:22 +0600

    another solution is added

--- 1008. Construct Binary Search Tree from Preorder Traversal.py
+++ 1008. Construct Binary Search Tree from Preorder Traversal.py
@@ -17,3 +17,23 @@ class Solution:
         return root
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
         return self.buildTree(preorder, float('inf'))
+#using simple preorder and inorder
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+   
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder.pop(0))
+        root_index = inorder.index(root.val)
+        root.left = self.buildTree(preorder, inorder[:root_index])
+        root.right = self.buildTree(preorder, inorder[root_index+1:])
+        return root
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        inorder = sorted(preorder)
+        return self.buildTree(preorder, inorder)
