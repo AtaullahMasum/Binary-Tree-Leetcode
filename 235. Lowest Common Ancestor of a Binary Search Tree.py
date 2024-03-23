274aee980a6374729570f6c6f0634acfc3fc3673
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-03-23 15:38:19 +0600

    LCA logn solution added

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -4,7 +4,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+#Time complexity is O(n)
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root or root == p or root == q:
@@ -17,4 +17,21 @@ class Solution:
             return left
         else:
             return root
+# these solution time complexity is O(logn)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return root
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
         
